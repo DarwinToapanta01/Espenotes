497bf93117e91f3b06cbc00b60489dcc56fdbc17
DarwinToapanta01 <datoapanta11@espe.edu.ec>
2025-07-29 12:37:06 -0500

    Primer commit EspeNotes

---
+++ src/js/app.js
@@ -0,0 +1,36 @@
+//DECLARACION DE VARIABLES GLOBALES
+let MAIN;
+let MODAL_POST;
+let BTN_UPLOAD_POST;
+let BTN_CLOSE_POST;
+
+//FUNCIONES
+const showpostmodal = () => {
+    MAIN.style.display = "none";
+    MODAL_POST.style.display = "block";
+    setTimeout(() => {
+        MODAL_POST.style.transform = "translateY(0)";
+    }, 1);
+};
+
+const hidepostmodal = () => {
+    MODAL_POST.style.transform = "translateY(100vh)";
+    setTimeout(() => {
+        MODAL_POST.style.display = "none";
+        MAIN.style.display = "block";
+    }, 300);
+};
+
+//CUANDO SE CARGA EL DOM
+window.addEventListener("load", () => {
+    MAIN = document.querySelector("#main");
+    MODAL_POST = document.querySelector("#modal-post-section");
+
+    // ✅ Apuntar correctamente al botón con el ícono "+"
+    BTN_UPLOAD_POST = document.querySelector("#btn-upload-post");
+    BTN_UPLOAD_POST.addEventListener("click", showpostmodal);
+
+    // ✅ Cerrar el modal al hacer clic en "Cancelar"
+    BTN_CLOSE_POST = document.querySelector("#btn-post-cancel");
+    BTN_CLOSE_POST.addEventListener("click", hidepostmodal);
+});
